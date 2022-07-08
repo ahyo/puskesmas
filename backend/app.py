01109432b11c3ea68b139111a52a4169fc3e89ab
Ahyo Haryanto <ahyo@id.mifx.com>
2022-07-08 11:17:50 +0700

    update

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail
+
+from database.db import initialize_db
+from flask_restful import Api
+from resources.errors import errors
 
 app = Flask(__name__)
-app.secret_key = "rahasia"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/puskesmas"
-mongo = PyMongo(app)
+app.config.from_envvar('ENV_FILE_LOCATION')
+mail = Mail(app)
+
+# imports requiring app and mail
+from resources.routes import initialize_routes
+
+api = Api(app, errors=errors)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+initialize_db(app)
+initialize_routes(api)
